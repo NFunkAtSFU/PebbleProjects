1fa83e28a4264f03e186df568afad5f428581f6e
Natalie Funk <nfunk@sfu.ca>
2019-03-15 00:01:07 -0700

    Initial commit

---
+++ natswatch/src/c/basicdisplay.c
@@ -0,0 +1,25 @@
+#include <pebble.h>
+
+static Window *s_main_window;
+
+static void main_window_load(Window *window) {
+	
+}
+
+static void main_window_unload(Window *window) {
+	
+}
+
+static void init() {
+	
+}
+
+static void deinit() {
+	
+}
+
+int main(void) {
+	init();
+	app_event_loop();
+	deinit();
+}
