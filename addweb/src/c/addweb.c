5ddbe7aad02d70ac53957f83db06ca1d440af2a6
Natalie Funk <nfunk@sfu.ca>
2019-03-23 00:57:51 -0700

    added weather lookup functionality

--- addweb/src/c/addweb.c
+++ addweb/src/c/addweb.c
@@ -1,5 +1,8 @@
 #include <pebble.h>
 
+#define KEY_TEMPERATURE 0
+#define KEY_CONDITIONS 1
+
 // static pointer to a Window variable, to access later in init()
 static Window *s_main_window;
 
@@ -7,10 +10,8 @@ static Window *s_main_window;
 static GFont s_time_font;
 static GFont s_weather_font;
 
-// use a TextLayer element to add to the Window
+// use a TextLayer element to add time and weather info to the Window
 static TextLayer *s_time_layer;
-
-// Adding Layer to hold weather information
 static TextLayer *s_weather_layer;
 
 // Pointers for bitmap
@@ -30,11 +31,6 @@ static void update_time() {
 	text_layer_set_text(s_time_layer, s_buffer);
 }
 
-// start TickTimerService event service. struct tm contains the current time
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-	update_time();
-}
-
 // handler function
 static void main_window_load(Window *window) {
 	// Get information about the Window
@@ -51,7 +47,7 @@ static void main_window_load(Window *window) {
 	bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
 	layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
 	
-	// Create the TextLayer with specific bounds
+	// Create the time TextLayer with specific bounds
 	s_time_layer = text_layer_create(
 		GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
 	
@@ -67,7 +63,7 @@ static void main_window_load(Window *window) {
 	text_layer_set_background_color(s_weather_layer, GColorClear);
 	text_layer_set_text_color(s_weather_layer, GColorWhite);
 	text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
-	text_layer_set_text(s_weather_layer, "Loading...");
+	// text_layer_set_text(s_weather_layer, "Loading...");
 
   // Create GFonts for time and for weather
     s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
@@ -82,7 +78,9 @@ static void main_window_load(Window *window) {
 	
 	// Add child layers to the Window's root layer
 	layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
-	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
+	layer_add_child(window_layer, text_layer_get_layer(s_weather_layer));
+	
+	update_time();
 	
 }
 
@@ -103,17 +101,68 @@ static void main_window_unload(Window *window) {
 	bitmap_layer_destroy(s_background_layer);
 }
 
+// start TickTimerService event service. struct tm contains the current time
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+	update_time();
+	
+	// Get weather update every 30 minutes
+	if(tick_time->tm_min % 30 == 0) {
+		// Begin dictionary
+		DictionaryIterator *iter;
+		app_message_outbox_begin(&iter);
+		
+		// Add a key-value pair
+		dict_write_uint8(iter, 0,0);
+		
+		// Send the message!
+		app_message_outbox_send();
+	}
+}
+
+// setting up callback functions for AppMessage
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+	
+	// Store incoming information from javascript weather
+	static char temperature_buffer[8];
+	static char conditions_buffer[32];
+	static char weather_layer_buffer[32];
+	
+	// Read tuples for data
+	Tuple *temp_tuple = dict_find(iterator, KEY_TEMPERATURE);
+	Tuple *conditions_tuple = dict_find(iterator, KEY_CONDITIONS);
+
+	// If all data is available, use it
+	if(temp_tuple && conditions_tuple) {
+	  snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int)temp_tuple->value->int32);
+	  snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", conditions_tuple->value->cstring);
+	  
+  	  // Assemble full string and display
+  	  snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+  	  text_layer_set_text(s_weather_layer, weather_layer_buffer);
+	}
+}
+
+// set up three callbacks for error messages
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+	APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+	APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed");
+}
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+	APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
 static void init() {
 	// Create main Window element and assign to pointer
 	s_main_window = window_create();
+	window_set_background_color(s_main_window, GColorBlack);
 	
 	// set handlers to manage the elements inside the Window
 	window_set_window_handlers(s_main_window, (WindowHandlers) {
 		.load = main_window_load,
 		.unload = main_window_unload
 	});
-	
-	window_set_background_color(s_main_window, GColorBlack);
 	
 	// Show the Window on the watch, with animated=true
 	window_stack_push(s_main_window, true);
@@ -123,6 +172,17 @@ static void init() {
 	
 	// Make sure the time is displayed from the start
 	update_time();
+	
+	// register callbacks for AppMessage
+	app_message_register_inbox_received(inbox_received_callback);
+	app_message_register_inbox_dropped(inbox_dropped_callback);
+	app_message_register_outbox_failed(outbox_failed_callback);
+	app_message_register_outbox_sent(outbox_sent_callback);
+	
+	// Open AppMessage
+	const int inbox_size = 128;
+	const int outbox_size = 128;
+	app_message_open(inbox_size, outbox_size);
 }
 
 static void deinit() {
